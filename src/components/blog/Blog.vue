299dd838f829829640d5fc45b06d6892f6758e16
Unknown <domenico.leopardi@gmail.com>
2018-03-07 16:24:38 -0500

    Created blog page

---
+++ src/components/blog/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="blog">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+  data () {
+    return {
+      msg: 'Blog'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
