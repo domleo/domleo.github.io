d9fd9f9aee4c6983edd83806d43df4fec4aa3108
Domenico <domenico.leopardi@gmail.com>
2018-03-12 23:37:54 -0400

    Added a bunch of social media icons in the page footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,6 +13,42 @@
         {{ link.name }}
       </v-btn>
       <v-flex xs12 py-3 text-xs-right white--text>
+        <a v-tooltip:top="{ html: 'Click to send me an email' }" href="mailto:domenico.leopardi@gmail.com" target="_blank">
+          <icon name="envelope" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view profile on GitHub' }" href="https://github.com/domleo/" target="_blank">
+          <icon name="github" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view profile on Linkedin' }" href="https://www.linkedin.com/in/domenicoleopardi" target="_blank">
+          <icon name="linkedin" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view my Youtube page' }" href="https://www.youtube.com/user/AnOtherLeopardi" target="_blank">
+          <icon name="youtube" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view my profile on Google+' }" href="https://plus.google.com/+DomenicoLeopardi" target="_blank">
+          <icon name="google-plus" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view my Tweetwe page' }" href="https://twitter.com/SnowLeopard789" target="_blank">
+          <icon name="twitter" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view profile on Facebook' }" href="https://www.facebook.com/domenico.leopardi" target="_blank">
+          <icon name="facebook" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <a v-tooltip:top="{ html: 'Click to view profile on Instagram' }" href="https://www.instagram.com/snowleopard_dom" target="_blank">
+          <icon name="instagram" scale="10"></icon>
+        </a>
+        &nbsp;&nbsp;
+        <!--a v-tooltip:top="{ html: 'Click to view profile on Instagram' }" href="https://www.workhoppers.com/fr/workhopper/45010/domenico" target="_blank">
+          <icon name="workhoppers" scale="10"></icon>
+        </a-->
+        &nbsp;&nbsp;
         &copy;2018 — <strong>Domenico Leopardi</strong>&nbsp;&nbsp;
       </v-flex>
     </v-layout>
@@ -33,5 +69,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+svg {
+    vertical-align: bottom;
+    color: white;
+    height: 25px;
+    width: 25px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vue-awesome/icons'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
+import Icon from 'vue-awesome/components/Icon.vue'
+// import SocialSharing from 'vue-social-sharing'
 
 Vue.use(Vuetify)
+// Vue.use(SocialSharing)
+Vue.component('icon', Icon)
 
 Vue.config.productionTip = false
 
