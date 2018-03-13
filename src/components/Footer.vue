03b8f9c18bbffbe1dfa4098e2e0da82b535268ac
Domenico <domenico.leopardi@gmail.com>
2018-03-13 15:44:36 -0400

    Better control of the paddings

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <div id='page-footer'>
-  <v-footer height="auto" class="grey darken-2">
+  <v-footer height="auto" class="grey darken-3">
     <v-layout row wrap justify-center>
+      <v-flex xs12 py-3 text-xs-center white--text>
       <v-btn
         color="white"
         flat
@@ -12,39 +13,32 @@
         ripple>
         {{ link.name }}
       </v-btn>
-      <v-flex xs12 py-3 text-xs-right white--text>
+      </v-flex>
+      <v-flex xs6 py-3 px-3 text-xs-left white--text>
         <a v-tooltip:top="{ html: 'Click to send me an email' }" href="mailto:domenico.leopardi@gmail.com" target="_blank">
           <icon class="fa fa-envelope" name="envelope" scale="10"></icon>
         </a>
-        &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view profile on GitHub' }" href="https://github.com/domleo/" target="_blank">
           <icon class="fa fa-github" name="github" scale="10"></icon>
         </a>
-          &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view profile on Linkedin' }" href="https://www.linkedin.com/in/domenicoleopardi" target="_blank">
           <icon class="fa fa-linkedin" name="linkedin" scale="10"></icon>
         </a>
-        &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view my Youtube page' }" href="https://www.youtube.com/user/AnOtherLeopardi" target="_blank">
           <icon class="fa fa-youtube" name="youtube" scale="10"></icon>
         </a>
-        &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view my profile on Google+' }" href="https://plus.google.com/+DomenicoLeopardi" target="_blank">
           <icon class="fa fa-google-plus" name="google-plus" scale="10"></icon>
         </a>
-        &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view my Tweetwe page' }" href="https://twitter.com/SnowLeopard789" target="_blank">
           <icon class="fa fa-twitter" name="twitter" scale="10"></icon>
         </a>
-        &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view profile on Facebook' }" href="https://www.facebook.com/domenico.leopardi" target="_blank">
           <icon class="fa fa-facebook" name="facebook" scale="10"/>
         </a>
-        &nbsp;&nbsp;
         <a v-tooltip:top="{ html: 'Click to view profile on Instagram' }" href="https://www.instagram.com/snowleopard_dom" target="_blank">
           <icon class="fa fa-instagram" name="instagram" scale="10"></icon>
         </a>
-        &nbsp;&nbsp;
         <!--a v-tooltip:top="{ html: 'Click to view profile on Instagram' }" href="https://www.workhoppers.com/fr/workhopper/45010/domenico" target="_blank">
           <icon name="workhoppers" scale="10"></icon>
         </a-->
@@ -74,6 +68,9 @@ svg {
   color: white;
   height: 25px;
   width: 25px;
+}
+a {
+  padding: 5px;
 }
 .fa {
   padding: 5px;
