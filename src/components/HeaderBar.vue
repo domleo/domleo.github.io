be38591812aff66e11843c0af550b32270888d08
Domenico <domenico.leopardi@gmail.com>
2018-03-20 14:27:24 -0400

    Created a list for the website links in the header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="header-bar">
-    <v-toolbar transparent class="grey darken-3">
-      <v-toolbar-title><router-link class="title" to="/">Domenico Leopardi</router-link></v-toolbar-title>
+    <v-toolbar transparent class="grey darken-3" flat height="65px">
+      <v-toolbar-title>
+        <router-link class="title" to="/">Domenico Leopardi</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <v-btn icon slot="activator" dark>
+          <icon class="fas fa-bars" name="bars" scale="5"></icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile color="white" v-for="link in links" :key="link.name" :to="link.url" :router="true">
+            <v-list-tile-title color="white">{{ link.name }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Headerbar'
+  name: 'Headerbar',
+  data: () => ({
+    links: [ {name: 'Home', url: '/'},
+      {name: 'About Me', url: '/aboutme'},
+      {name: 'Blog', url: '/blog'},
+      {name: 'Contact', url: '/contact'} ]
+  })
 }
 </script>
 
@@ -19,5 +38,30 @@ export default {
 .title {
   color: white;
   text-decoration: none;
+}
+.fa-bars {
+  color: white;
+}
+svg {
+  vertical-align: bottom;
+  color: white;
+  height: 25px;
+  width: 25px;
+}
+a {
+  color: white;
+  padding: 5px;
+}
+.primary--text {
+    color: white !important;
+}
+.fa {
+  padding: 5px;
+  font-size: 40px;
+  height: 40px;
+  width: 40px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
 }
 </style>
