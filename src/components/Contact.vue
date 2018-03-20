1f0805a5260ff9ff149f7c2c0dcd3f6fd51835a8
Domenico <domenico.leopardi@gmail.com>
2018-03-20 14:50:29 -0400

    Added information to contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,6 +2,16 @@
     <div class="homepage">
         <v-container>
             <h1>Contact</h1>
+            <p>
+              The best way to contact me is via e-mail at <a href="mailto:domenico.leopardi@gmail.com">domenico.leopardi@gmail.com</a>.
+            </p>
+            <p>
+                If you prefer to speak to me instead, you may call or leave a message at: <a href="tel:1-514-799-5699"> 1-514-799-5699 </a>
+            </p>
+            <p>
+                Or you may e-mail your coordinates and what time I may reach you at. I will get back to you as soon as I can.
+            </p>
+            <h2>Hope to hear from you soon!</h2>
         </v-container>
     </div>
 </template>
@@ -17,8 +27,12 @@ export default {
   text-align: center;
   margin-top: 25px;
  }
-h1, h2 {
+h1, h2, p {
   font-weight: normal;
+  color: #f8f8f8;
+  text-align: left;
+}
+a {
   color: #f8f8f8;
 }
 </style>
